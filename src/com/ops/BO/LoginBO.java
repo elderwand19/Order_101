9e9c49b5caee6e1a16b42f7b5d3cd9083fd641f4
Shivam Shakya <46165706+elderwand19@users.noreply.github.com>
2022-09-25 18:19:55 +0530

    Delete src/com/ops directory

--- src/com/ops/BO/LoginBO.java
+++
@@ -1,9 +0,0 @@
-package com.ops.BO;
-
-import com.ops.model.Customer;
-import com.ops.model.Employee;
-
-public interface LoginBO {
-	public String customerLogin(String email,String password);
-	public String employeeLogin(String empEmail,String empPassword);
-}
